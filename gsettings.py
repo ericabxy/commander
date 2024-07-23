bf34b08fc41054eb88035ef486cc2361f81780a8
Erekir <erekir@unknown426c61dac6b4.attlocal.net>
2024-07-23 07:00:19 -0700

    move specific methods inside objects

--- gsettings.py
+++ gsettings.py
@@ -1,13 +1,21 @@
 #/usr/bin/env python
+"""Tkinter frontend to the GNOME settings command.
+
+The interface consists of three objects: the SCHEMAS list, the KEYS
+list, and the currently selected VALUE. Selecting a schema and a key
+determines what value is currently selected.
+"""
 import subprocess
 
 from tkinter import *
 from tkinter import messagebox, ttk
 
 GSETTINGS = 'gsettings'
+LISTSCHEMAS = 'list-schemas'
+LISTKEYS = 'list-keys'
 
 class Schemas(Frame):
-	def __init__(self, main):
+	def __init__(self, main, **kwargs):
 		super().__init__(main)
 		self.treeview = ttk.Treeview(self, selectmode=BROWSE)
 		self.scrollbar = Scrollbar(self, command=self.treeview.yview)
@@ -17,16 +25,15 @@ class Schemas(Frame):
 		self.scrollbar.pack(side=LEFT)
 		self.pack(side=TOP, anchor=NW)
 		self.populate()
-
-	def bind(self, event, command):
-		self.treeview.bind(event, command)
+		if 'command' in kwargs:
+			self.treeview.bind('<<TreeviewSelect>>', kwargs['command'])
 
 	def populate(self):
 		self.schemas.clear()
 		output = subprocess.run(
 			(
-				GSETTINGS,
-				'list-schemas'
+				'gsettings',
+				LISTSCHEMAS
 			),
 			capture_output = True
 		)
@@ -48,188 +55,134 @@ class Schemas(Frame):
 	def selection(self):
 		return self.treeview.selection()[0]
 
+	def item(self):
+		item_id = self.treeview.selection()[0]
+		if item_id in self.schemas:
+			return self.schemas[item_id]
 
-class Keys(Listbox):
-	def __init__(self, main):
+
+class Keys(Frame):
+	def __init__(self, main, **kwargs):
 		super().__init__(main)
-		self.pack(side=TOP, anchor=NW)
+		self.listbox = Listbox(self)
+		self.listbox.pack()
+		self.pack(side=LEFT, anchor=NW)
+		if 'command' in kwargs:
+			self.listbox.bind('<<ListboxSelect>>', kwargs['command'])
+
+	def item(self):
+		if self.listbox.curselection():
+			index = self.listbox.curselection()[0]
+			return self.listbox.get(index)
 
 	def populate(self, schema):
+		if not isinstance(schema, str):
+			return
 		self.schema = schema
 		output = subprocess.run(
 			(
-				GSETTINGS,
-				'list-keys',
+				'gsettings',
+				LISTKEYS,
 				schema
 			),
 			capture_output = True
 		)
 		keys = output.stdout.decode('utf-8').split()
-		self.delete(0, self.size())
-		self.insert(0, *keys)
+		self.listbox.delete(0, self.listbox.size())
+		self.listbox.insert(0, *keys)
 
 
-class Info(Frame):
+class Value(Frame):
 	def __init__(self, main):
 		super().__init__(main)
 		self.description = Label(self, wraplength=250)
-		self.description.pack()
 		self.range = Label(self)
-		self.range.pack()
 		self.value = Label(self)
+		self.spinval = StringVar(self, 'none')
+		self.spinbox = ttk.Entry(self, textvariable=self.spinval)
+		self.button1 = ttk.Button(
+			self,
+			text='Reset',
+			command=self.reset
+		)
+		self.button2 = ttk.Button(
+			self,
+			text='Set',
+			command=self.set
+		)
+		self.description.pack()
+		self.range.pack()
 		self.value.pack()
-		self.pack()
+		self.spinbox.pack()
+		self.button1.pack()
+		self.button2.pack()
+		self.pack(side=LEFT)
+
+	def reset(self):
+		output = subprocess.run(
+			('gsettings', 'reset', self.schema, self.key)
+		)
+		self.show(self.schema, self.key)
+
+	def set(self):
+		value = self.spinval.get()
+		output = subprocess.run(
+			('gsettings', 'set', self.schema, self.key, value)
+		)
+		self.show(self.schema, self.key)
 
 	def show(self, schema, key):
+		if not schema or not key:
+			return
+		self.schema = schema
+		self.key = key
 		describe = subprocess.run(
-			(GSETTINGS, 'describe', schema, key),
+			('gsettings', 'describe', schema, key),
 			capture_output = True
 		)
 		range = subprocess.run(
-			(GSETTINGS, 'range', schema, key),
+			('gsettings', 'range', schema, key),
 			capture_output = True
 		)
 		value = subprocess.run(
-			(GSETTINGS, 'get', schema, key),
+			('gsettings', 'get', schema, key),
 			capture_output = True
 		)
 		self.description.configure(text=describe.stdout.decode('utf-8'))
 		self.range.configure(text=range.stdout.decode('utf-8'))
 		self.value.configure(text=value.stdout.decode('utf-8'))
-
-
-class Value(Frame):
-	"""Show and set the value of a key."""
-	def __init__(self, main):
-		super().__init__(main)
-		self.spinval = StringVar(self, 'none')
-		self.spinbox = ttk.Entry(self, textvariable=self.spinval)
-		self.spinbox.pack()
-		self.pack()
-
-	def range_get(self, schema, key):
-		self.schema = schema
-		self.key = key
-		range = subprocess.run(
-			(GSETTINGS, 'range', schema, key),
-			capture_output = True
-		)
-		get = subprocess.run(
-			(GSETTINGS, 'get', schema, key),
-			capture_output = True
-		)
-		range = range.stdout.decode('utf-8')
-		self.spinval.set(get.stdout.decode( 'utf-8' ))
-		if range[:7] == 'range i':
-			pass
-		elif range[:6] == 'type i':
-			pass
-		elif range[:6] == 'type b':
-			pass
-
-	def set(self):
-		value = self.spinval.get()
-		output = subprocess.run(
-			(GSETTINGS, 'set', self.schema, self.key, value)
-		)
+		self.spinval.set(value.stdout.decode( 'utf-8' ))
 
 
 class Application(Frame):
 	def __init__(self, main):
 		Frame.__init__(self, main)
 		self.frame1 = ttk.Frame(self)
-		self.frame1.pack(side=LEFT)
 		self.frame2 = ttk.Frame(self)
+		self.schemas = Schemas(self.frame1, command=self.select_schema)
+		self.keys = Keys(self.frame1, command=self.select_key)
+		self.value = Value(self.frame2)
+		self.frame1.pack(side=LEFT)
 		self.frame2.pack(side=LEFT)
-		self.treeview = Schemas(self.frame1)
-		self.listbox = Keys(self.frame1)
-		self.treeview.bind('<<TreeviewSelect>>', self.list_keys)
-		self.listbox.bind('<<ListboxSelect>>', self.information)
-		self.description = Info(self.frame2)
-		self.setting = Value(self.frame2)
-		self.button = ttk.Button(
-			self.frame2,
-			text='Set',
-			command=self.setting.set
+
+	def select_schema(self, event):
+		self.keys.populate(
+			self.schemas.item()
 		)
-		self.button.pack()
-		self.process = GSETTINGS
-		self.command = 'help'
-		self.schema = ''
-		self.key = ''
-		self.value = ''
 
-	def set_value(self):
-		self.setting.set()
-		self.description.show()
-
-	def reset_value(self):
-		self.setting.reset()
-		self.description.show()
-
-	def list_keys(self, event):
-		item_id = self.treeview.selection()
-		if item_id not in self.treeview.schemas:
-			return
-		self.listbox.populate(self.treeview.retrieve( item_id ))
-
-	def get_range(self):
-		self.command = 'range'
-		output = subprocess.run(
-			(
-			GSETTINGS,
-			'range',
-			self.schema,
-			self.key
-			),
-			capture_output = True
-		)
-		range = output.stdout.decode('utf-8')
-		if range[:7] == 'range i':
-			low = range.split()[2]
-			high = range.split()[3]
-			self.range_i.configure(from_=low, to=high)
-			self.get_value(int)
-
-	def information(self, event):
-		if not self.listbox.curselection():
-			return
-		item = self.listbox.curselection()[0]
-		key = self.listbox.get(item)
-		self.description.show(self.listbox.schema, key)
-		self.setting.range_get(self.listbox.schema, key)
-
-	def get_value(self, cast):
-		self.command = 'get'
-		output = subprocess.run(
-			(
-			self.process,
-			self.command,
-			self.schema,
-			self.key
-			),
-			capture_output = True
-		)
-		value = output.stdout.decode('utf-8')
-		self.spinval = cast( value )
-
-	def set_value(self):
-		self.command = 'set'
-		subprocess.run(
-			(
-			self.process,
-			self.command,
-			self.schema,
-			self.key
-			)
+	def select_key(self, event):
+		self.value.show(
+			self.schemas.item(),
+			self.keys.item()
 		)
 
 	def quit(self, event=None):
 		sys.exit()
 
+
 root = Tk()
 root.geometry('640x480')
-root.title("GNOME Settings Frontend")
+root.title("GSettings Frontend")
 
 Application(root).pack()
 
