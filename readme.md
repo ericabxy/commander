97dfc0321da9d48cdcdf97c39188673a2b798c45
Erekir <erekir@unknown426c61dac6b4.attlocal.net>
2024-07-23 09:17:49 -0700

    cleanup files

--- readme.md
+++
@@ -1,4 +0,0 @@
-Commander
-=========
-
-Command-line constructor
--- schemas.py
+++
@@ -1,8 +0,0 @@
-import subprocess
-
-g_out = subprocess.run(('gsettings', 'list-schemas'), capture_output=True)
-schemas = g_out.stdout.decode('utf-8').split()
-
-for schema in schemas:
-	first = schema.split('.')[0]
-	print(first)
